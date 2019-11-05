751722a972238395d69ae7aa240a70ff1f6e18dc
Stephen Bennett <stebennettsjb@gmail.com>
2019-11-05 22:23:23 +0000

    contact-form
    intro contact form
    
    - need to remove hardcoded api key
    - need to hardcode emailTo
    - need to remove send from
    - need to update message to include From

--- site/Controllers/HomeController.cs
+++ site/Controllers/HomeController.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using site.Models;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using static site.Models.EmailModel;
 
 namespace site.Controllers
 {
@@ -36,10 +39,40 @@ namespace site.Controllers
             return View();
         }
 
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SendgridEmailSubmit(EmailModel emailmodel)
+        {
+            ViewData["Message"] = "Email Sent!!!...";
+            SendGrid emailexample = new SendGrid();
+            await emailexample.Execute(emailmodel.From, emailmodel.To, emailmodel.Subject, emailmodel.Body
+                , emailmodel.Body);
+
+            return View("Index");
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+    internal class SendGrid
+    {
+        public async Task Execute(string From, string To, string subject, string plainTextContent, string htmlContent)
+        {
+            var apiKey = "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";
+            
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress(From);
+            var to = new EmailAddress(To);
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
         }
     }
 }
