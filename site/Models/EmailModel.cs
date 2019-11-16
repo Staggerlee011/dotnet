32fd98b08f4f2235d6d8fbeea7b3fdc868b3354b
Stephen Bennett <stebennettsjb@gmail.com>
2019-11-16 20:35:31 +0000

    contact form validation

--- site/Models/EmailModel.cs
+++ site/Models/EmailModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,24 @@ namespace site.Models
 {
     public class EmailModel
     {
-            public string From { get; set; }
-            public string To { get; set; }
-            public string Subject { get; set; }
-            public string Body { get; set; }
+        [Required(ErrorMessage = "Please enter your email address")]
+        [EmailAddress]
+        [Display(Name = "From Email Address")]
+        public string From { get; set; }
+        
+        [Required(ErrorMessage = "Please enter your email address")]
+        [EmailAddress]
+        [Display(Name = "To Email Address")]
+        public string To { get; set; }
+
+        [Required(ErrorMessage = "Subject for the message")]
+        [MinLength(5)]
+        [MaxLength(20)]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "Dont you want to send a message?")]
+        [MinLength(5)]
+        [MaxLength(1024)]
+        public string Body { get; set; }
     }
 }
